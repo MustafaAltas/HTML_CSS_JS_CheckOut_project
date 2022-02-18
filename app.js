74081267f6afb4aad17dda280d5f61a6b00865a5
D1183-Mustafa <93746275+D1183-Mustafa@users.noreply.github.com>
2022-02-18 06:44:49 +0300

    değişikler yapıldı

--- app.js
+++ app.js
@@ -21,12 +21,13 @@ for (let i = 0; i < artıEksi.length; i++) {
           ) *
           (miktarArtış + 1)
         ).toFixed(2);
-
-
     } else if (e.target.className === "button-azalış") {
       miktarAzalış = e.target.parentElement.querySelector(".miktar")
         .innerHTML--;
-      if (miktarAzalış < 1) {
+      e.target.parentElement.parentElement.querySelector(".p-total").innerHTML = (parseFloat(e.target.parentElement.parentElement.querySelector(".p-total").innerHTML) - parseFloat(e.target.parentElement.parentElement.querySelector(".container-div-span-orange").innerHTML)).toFixed(2);
+
+
+      if (miktarAzalış == 1) {
         e.target.parentElement.querySelector(".miktar").innerText = 1;
       }
     }
